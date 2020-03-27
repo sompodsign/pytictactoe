56e1aabe51cb78bb7c8a9b91c1acd5800397e7c8
Shampad Sharkar <sompodsign@gmail.com>
2020-03-28 03:40:10 +0600

    board designed

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -5,9 +5,16 @@
 
 def display_board(board):
     print('   |   |   ')
-    print(f' {board[0]} | {board[1]} | {board[2]}')
+    print(f' {board[7]} | {board[8]} | {board[9]}')
     print('   |   |   ')
     print('-----------')
+    print('   |   |   ')
+    print(f' {board[4]} | {board[5]} | {board[6]}')
+    print('   |   |   ')
+    print('-----------')
+    print('   |   |   ')
+    print(f' {board[1]} | {board[2]} | {board[3]}')
+    print('   |   |   ')
 
 
 
